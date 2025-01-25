41aef0aa53561f6a536da5c9f5ad3d88dd4e83dd
Paul B <paulberger56@gmail.com>
2025-01-25 16:51:59 -0500

    Add dynamic navigators and stack navigator

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import Colors from '../constants/Colors';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 export const defaultPizzaImage = 
     'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
@@ -11,14 +12,18 @@ type ProductListItemProps = {
 
 const ProductListItem = ({ product }: ProductListItemProps) => {  
   return (
-    <View style={styles.container}>
-        <Image source={{ uri: product.image || defaultPizzaImage }}
-          style={styles.Image}
-          resizeMode='contain' 
-        />
-        <Text style={styles.title}>{product.name}</Text>
-        <Text style={styles.price}>${product.price}</Text>
-    </View>
+    // use backticks "`", not single quotes "'" for object references in the link 
+    <Link href={`/menu/${product.id}`} asChild> 
+      <Pressable style={styles.container}>
+          <Image source={{ uri: product.image || defaultPizzaImage }}
+            style={styles.Image}
+            resizeMode='contain' 
+          />
+
+          <Text style={styles.title}>{product.name}</Text>
+          <Text style={styles.price}>${product.price}</Text>       
+      </Pressable>
+    </Link>
   );
 }
 
